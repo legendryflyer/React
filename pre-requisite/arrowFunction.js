92c7fcdfc84c61469b82a70cf12f9b688a16393f
legendryflyer <tavish.anade@gmail.com>
2024-04-07 09:59:37 +0530

    added

--- pre-requisite/arrowFunction.js
+++ pre-requisite/arrowFunction.js
@@ -1,14 +1,51 @@
-let birthyear = [2000,2001,2002,2003,2004,2005]
+// let birthyear = [2000,2001,2002,2003,2004,2005]
 
-age = birthyear.Map(function(el,index,Array){
-    return 2024 - el
-})
+// age = birthyear.Map(function(el,index,Array){
+//     return 2024 - el
+// })
 
-console.log(age)
+// console.log(age)
 
 
-age2=birthyear.Map(el=> 2024-el)
-console.log(age2)
+// age2=birthyear.Map(el=> 2024-el)
+// console.log(age2)
+
+
+
+// let tav = {
+//     firstName:"Tavish",
+//     lastName:"anade",
+//     age:20,
+//     displayName(){
+//         console.log(`${this.firstName} ${this.lastName}`)
+
+//         let displayName2 = function(){
+//             console.log(`${this.firstName}  ${this.lastName}`)  // no scope for this  so it will refer to the window object
+//         }
+//         displayName2()
+
+
+//     }
+// }
+// tav.displayName()
+
+
+let tav = {
+    firstName:"Tavish",
+    lastName:"anade",
+    age:20,
+    displayName(){
+        console.log(`${this.firstName} ${this.lastName}`)
+
+        let displayName2 = ()=>{
+            console.log(`${this.firstName}  ${this.lastName}`)  
+        }
+        displayName2()
+
+
+    }
+}
+tav.displayName()
 
 
 
