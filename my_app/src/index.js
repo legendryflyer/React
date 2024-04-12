b6bd6808df8dfd7832e0ec550268db57300a279e
legendryflyer <tavish.anade@gmail.com>
2024-04-12 11:44:10 +0530

    added

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -2,10 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-function App(){
-  return (<h1>Learning React</h1>)
+function App() {
+  return (
+    <>
+      <h1>learning react</h1>
+      <About />
+      <Header />
+      <Menu />
+      <Footer />
+    </>)
 }
 
+
+function About() {
+  return (<h2>About</h2>)
+}
+
+function Header() {
+  return (<h3>Header</h3>)
+}
+
+function Menu() {
+  return (<h4>manu</h4>)
+}
+
+function Footer() {
+  return (<h3>Footer</h3>)
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+root.render(<App />)
 // root.render(App())
