20facd41d0d06a6641cd874179853673ce5327ae
legendryflyer <tavish.anade@gmail.com>
2024-04-16 20:23:38 +0530

    added

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,68 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-let data = [{
-  name: "pizza spinach",
-  price: 120,
-  size: "medium"
-},
-{
-  name: "pizza margherita",
-  price: 85,
-  size: "small"
-},
-{
-  name: "calzone",
-  price: 65,
-  size: "large"
-}]
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
 
 function App() {
   return (
-    <>
-      <h1>learning react</h1>
-      <About />
+    <div>
       <Header />
       <Menu />
       <Footer />
-    </>)
-}
-
-
-function About() {
-  return (<h2>About</h2>)
-}
-
-function Header() {
-  return (<h3>Header</h3>)
-}
-
-function Menu() {
-  return (
-    <>
-      <h4>manu</h4>
-      <Pizza/>
-    </>
-  )
-}
-
-function Pizza() {
-  let style = { color: 'red', backgroundColor: 'yellow' }
-  return (
-    <div>
-      <p style={style}>I love pizza! </p>
-      <p style={style}>{data[0].price}</p>
-      <p style={style}>{data[0].size}</p>
     </div>
   )
 }
 
+
+function Menu() {
+  return (
+    <>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  );
+}
+
+function Pizza() {
+  return (
+    <>
+      <div>
+        <img src="images\focaccia.jpg" alt="focaccia" />
+        <h2>Focaccia with mozzarella and basil.</h2>
+        <p>15 €</p>
+      </div>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <header className="App-header">Welcome to pizzaRia</header>
+    </>
+  );
+}
+
 function Footer() {
-  return (<h3>Footer</h3>)
+  return (
+    <>
+      <footer className='App-footer'>pizzaRia-(since_2024)</footer>
+    </>
+  );
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
-// root.render(App())
+
+
