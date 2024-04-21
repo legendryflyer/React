25c572856653b0e3182a1658c8544bddeddb4fc6
legendryflyer <tavish.anade@gmail.com>
2024-04-21 14:31:57 +0530

    day3

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -46,9 +46,24 @@ function Header() {
 }
 
 function Footer() {
+  const hour = new Date().getHours()+5
+  const openHour = 12
+  const closeHour = 22
+  const isOpen = hour >= openHour && hour  <= closeHour
+  console.log(isOpen)
+
+  function Order({closeHour,openHour}){
+    return(
+    <>
+      <p>We are open from {openHour}:00 to {closeHour}:00 today!</p>
+    </>
+    )
+  }
   return (
     <>
-      <footer className='App-footer'>pizzaRia-(since_2024)</footer>
+      <footer className='App-footer'>pizzaRia-(since_2024)
+        {isOpen?(<Order closeHour={closeHour} openHour={openHour}></Order>):(<p>We are happy to welcome you between our open hr{openHour}:00 and {closeHour}:00</p>)}
+      </footer>
     </>
   );
 }
