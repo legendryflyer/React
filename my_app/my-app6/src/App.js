5380704da47cb6b5804622850bd60c76c06661cc
legendryflyer <tavish.anade@gmail.com>
2024-05-23 19:45:25 +0530

    day14

---
+++ my_app/my-app6/src/App.js
@@ -0,0 +1,65 @@
+import "./index.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <FlashCards /> */}
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Steps:{step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+
+        <p>
+          <span>
+            {count === 0
+              ? "Today is"
+              : count > 0
+              ? `${count} days from today is`
+              : `${count} days ago was`}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
+
+        {count !== 0 || step !== 1 ? (
+          <div>
+            <button onClick={handleReset}>Reset</button>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
