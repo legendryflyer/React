a7523d20beac84d8f72ba9057155c5f295b33f47
legendryflyer <tavish.anade@gmail.com>
2024-06-06 20:07:32 +0530

    day 18

--- my_app/my-app9/src/components/TipCalculator.js
+++ my_app/my-app9/src/components/TipCalculator.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import BillInput from "./BillInput";
 import SelectPerCentage from "./SelectPerCentage";
 import Output from "./Output";
 import Reset from "./Reset";
 
 export default function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [persent1, setPersent1] = useState(0);
+  const [persent2, setPersent2] = useState(0);
+  const tip = bill * ((persent1 + persent2) / 2 / 100);
+
+  function reset() {
+    setBill("");
+    setPersent1(0);
+    setPersent2(0);
+  }
   return (
     <div>
-      <BillInput />
-      <SelectPerCentage>How did you like my service?</SelectPerCentage>
-      <SelectPerCentage>How did your friend like my service?</SelectPerCentage>
-      <Output />
-      <Reset />
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPerCentage percentage={percent1} onselect={setPersent1}>
+        How did you like my service?
+      </SelectPerCentage>
+      <SelectPerCentage percentage={percent2} onselect={setPersent2}>
+        How did your friend like my service?
+      </SelectPerCentage>
+      <Output bill={bill} tip={tip} />
+      <Reset onReset={reset} />
     </div>
   );
 }
