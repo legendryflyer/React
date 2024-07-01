fbd27d6693e0992422909f57580d2855aa44bdfd
legendryflyer <tavish.anade@gmail.com>
2024-07-01 21:00:58 +0530

    added

--- pre-requisite/shortCircuiting.js
+++ pre-requisite/shortCircuiting.js
@@ -1,6 +1,6 @@
 // && - and
 
-let a = 7 == 8 && true; // in the case of && if output of first expression is false it will short circuit. It won't evaluate second part.
+let a = 7 == 8 && true; // in the case of && if output of first expression is falsy it will short circuit. It won't evaluate second part.
 console.log(a);
 
 let b = 7 == 7 && false;
@@ -12,7 +12,7 @@ console.log(c); // hellow
 let d = 7 > 8 && "hellow";
 console.log(d); // false
 
-//  || - or
+//  || - or // in thr case of or if the output of first expression is truthly it will short circuit. It won't evaluate the second part..
 
 let q13 = 7 != 7 || 8 != 9; // if  one of them is true it will return true
 console.log(q13); //true
