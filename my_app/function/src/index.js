a106d8d6ab0e1586ed4ec0cfd13f99606faf7e3d
legendryflyer <tavish.anade@gmail.com>
2024-07-23 20:18:07 +0530

    day21

--- my_app/function/src/index.js
+++ my_app/function/src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import Function1 from "./Function1";
-// import Function2 from "./Function2";
+
 import Function3 from "./Function3";
-// import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Function1 /> */}
-    {/* <Function2 /> */}
     <Function3 />
   </React.StrictMode>
 );
